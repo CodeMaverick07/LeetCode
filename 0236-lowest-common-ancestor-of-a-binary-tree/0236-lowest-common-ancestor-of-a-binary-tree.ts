b1216610ea822e0eabc1f93db9b81882a51fb875
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2025-01-07 18:37:53 +0530

    Time: 924 ms (5.12%), Space: 59.7 MB (63.82%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+   if (root == p || root == q) return root 
+   if (p==q) return p
+
+let left = contains(root.left,p)
+let right = contains(root.right,q)
+if((left&&right )|| (!left&&!right)) return root
+if (left && !right) return lowestCommonAncestor(root.left,p,q)
+if (!left&&right) return lowestCommonAncestor(root.right,p,q)
+return null
+	
+};
+
+function contains(root,node){
+    if (root == node) return true
+    if (root == null ) return false
+    return contains(root.left,node)|| contains(root.right,node)
+
+}
