86baa5d239fc2a8bbf11d12c2b7d0d484f1747d0
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2025-01-08 13:16:19 +0530

    Time: 1 ms (24.34%), Space: 52.2 MB (39.88%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.ts
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.ts
@@ -1,57 +1,40 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val === undefined ? 0 : val);
- *         this.left = (left === undefined ? null : left);
- *         this.right = (right === undefined ? null : right);
- *     }
- * }
- */
-
 function zigzagLevelOrder(root: TreeNode | null): number[][] {
-  if (root === null) return [];
-
-  let level = height(root);
-  let ans: number[][] = [];
-
-  for (let i = 1; i <= level; i++) {
-    let arr: number[] = [];
-    if (i % 2 !== 0) {
-      nthlevel(root, i, arr); // Left to right
-    } else {
-      nthlevelReverse(root, i, arr); // Right to left
+    if (root == null) return [];
+    let level = height(root);
+    let ans: number[][] = [];
+    for (let i = 1; i <= level; i++) {
+        let arr: number[] = [];
+        if (i % 2 !== 0) {
+            nthlevel(root, i, arr); 
+        } else {
+            nthlevel2(root, i, arr); 
+        }
+        ans.push(arr);
     }
-    ans.push(arr);
-  }
-
-  return ans;
+    return ans;
 }
 
 function height(root: TreeNode | null): number {
-  if (root === null) return 0;
-  return 1 + Math.max(height(root.left), height(root.right));
+    if (root == null) return 0;
+    return 1 + Math.max(height(root.left), height(root.right));
 }
 
 function nthlevel(root: TreeNode | null, n: number, arr: number[]) {
-  if (root === null) return;
-  if (n === 1) {
-    arr.push(root.val);
-    return;
-  }
-  nthlevel(root.left, n - 1, arr);
-  nthlevel(root.right, n - 1, arr);
+    if (root == null) return;
+    if (n == 1) {
+        arr.push(root.val);
+        return;
+    }
+    nthlevel(root.left, n - 1, arr);
+    nthlevel(root.right, n - 1, arr);
 }
 
-function nthlevelReverse(root: TreeNode | null, n: number, arr: number[]) {
-  if (root === null) return;
-  if (n === 1) {
-    arr.push(root.val);
-    return;
-  }
-  nthlevelReverse(root.right, n - 1, arr);
-  nthlevelReverse(root.left, n - 1, arr);
+function nthlevel2(root: TreeNode | null, n: number, arr: number[]) {
+    if (root == null) return;
+    if (n == 1) {
+        arr.push(root.val);
+        return;
+    }
+    nthlevel2(root.right, n - 1, arr);
+    nthlevel2(root.left, n - 1, arr);
 }
