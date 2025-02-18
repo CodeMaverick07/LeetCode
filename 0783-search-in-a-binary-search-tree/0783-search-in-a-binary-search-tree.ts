848b0ff6d826ad6cb7d68f167eca0656d3a272b1
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2025-02-19 00:21:05 +0530

    Time: 0 ms (100%), Space: 62.8 MB (6.85%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.ts
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.ts
@@ -13,8 +13,9 @@
  */
 
 function searchBST(root: TreeNode | null, val: number): TreeNode | null {
-    if (root == null ) return null
-      if (root.val == val) return root 
-      if ( root.val < val) return searchBST(root.right,val)
-      else return searchBST(root.left,val)
+    if (root == null) return null
+    if (root.val == val) return root
+    if (root.val > val ) return searchBST(root.left,val)
+    if (root.val < val ) return searchBST(root.right,val)
+    return root
 };
