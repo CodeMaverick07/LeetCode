d6845217497d39562e5e041ca54f79ddcddc6fd4
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2025-02-20 21:20:00 +0530

    Time: 0 ms (100%), Space: 55.6 MB (21.05%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.ts
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.ts
@@ -1,26 +1,32 @@
+// function findDifferentBinaryString(nums: string[]): string {
+//     const n = nums.length;
+//     const seen = new Set(nums);
+//     let result = '';
+    
+//     function dfs(curr: string) {
+//         if (curr.length === n) {
+//             if (!seen.has(curr)) {
+//                 result = curr;
+//             }
+//             return;
+//         }
+//         for (const bit of ['0', '1']) {
+//             if (!result) {
+//                 dfs(curr + bit);
+//             }
+//         }
+//     }
+    
+//     dfs('');
+//     return result;
+// }
 function findDifferentBinaryString(nums: string[]): string {
-    const n = nums.length;
-    const seen = new Set(nums);
     let result = '';
     
-    function dfs(curr: string) {
-        // If we found a string not in nums, store it and return
-        if (curr.length === n) {
-            if (!seen.has(curr)) {
-                result = curr;
-            }
-            return;
-        }
-        
-        // Try both 0 and 1 for each position
-        for (const bit of ['0', '1']) {
-            // If we haven't found a result yet, continue searching
-            if (!result) {
-                dfs(curr + bit);
-            }
-        }
+    // Flip the bit at each position i from nums[i]
+    for (let i = 0; i < nums.length; i++) {
+        result += nums[i][i] === '0' ? '1' : '0';
     }
     
-    dfs('');
     return result;
 }
