b6ee02b3ee8484fa3845b35b7b226e69f3b582d1
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2025-03-03 17:11:24 +0530

    Time: 37 ms (50%), Space: 89.6 MB (21.67%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.ts
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.ts
@@ -1,22 +1,7 @@
 function pivotArray(nums: number[], pivot: number): number[] {
-    let n = nums.length
-       let index = nums.indexOf(pivot)
-       let small = []
-       let arr = []
-       let large = []
-    for (let i = 0; i < n; i++){
-          if (nums[i] == pivot){
-            arr.push(pivot)
-          }
-          if (nums[i]<pivot){
-            small.push(nums[i])
-          }
-          if (nums[i]>pivot){
-            large.push(nums[i])
-          }
-    }
-    
-    small = small.concat(arr)
-    small = small.concat(large)
-    return small
+    const ans = []
+    for(const n of nums) if(n < pivot) ans.push(n)
+    for(const n of nums) if(n === pivot) ans.push(n)
+    for(const n of nums) if( n > pivot) ans.push(n)
+    return ans
 };
