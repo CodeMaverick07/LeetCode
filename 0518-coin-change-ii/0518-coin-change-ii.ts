bc9e0ccd73e7bdad9b288dca2ad95feef74bd0e3
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2025-03-05 15:19:37 +0530

    Time: 7 ms (96.84%), Space: 59.4 MB (50%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.ts
+++ 0518-coin-change-ii/0518-coin-change-ii.ts
@@ -1,29 +1,27 @@
-function change(amount: number, coins: number[]): number {
-    let n = coins.length;
-    let dp = Array.from({ length: n }, () => new Array(amount + 1).fill(0));
+// function change(amount: number, coins: number[]): number {
+//     let n = coins.length;
+//     let dp = Array.from({ length: n }, () => new Array(amount + 1).fill(0));
+//     for (let i = 0; i < n; i++) {
+//         dp[i][0] = 1;
+//     }
+//     for (let i = 0; i <= amount; i++) {
+//         if (i % coins[0] === 0) {
+//             dp[0][i] = 1;
+//         }
+//     }
+//     for (let i = 1; i < n; i++) {
+//         for (let sum = 0; sum <= amount; sum++) {
+//             let pick = 0;
+//             if (sum >= coins[i]) {
+//                 pick = dp[i][sum - coins[i]];
+//             }
+//             let notPick = dp[i - 1][sum];
+//             dp[i][sum] = notPick + pick;
+//         }
+//     }
 
-   
-    for (let i = 0; i < n; i++) {
-        dp[i][0] = 1;
-    }
-    for (let i = 0; i <= amount; i++) {
-        if (i % coins[0] === 0) {
-            dp[0][i] = 1;
-        }
-    }
-    for (let i = 1; i < n; i++) {
-        for (let sum = 0; sum <= amount; sum++) {
-            let pick = 0;
-            if (sum >= coins[i]) {
-                pick = dp[i][sum - coins[i]];
-            }
-            let notPick = dp[i - 1][sum];
-            dp[i][sum] = notPick + pick;
-        }
-    }
-
-    return dp[n - 1][amount];
-}
+//     return dp[n - 1][amount];
+// }
 // function change(amount: number, coins: number[]): number {
 //     let memo: Map<string, number> = new Map(); 
 //     return helper(coins, amount, coins.length - 1, memo);
@@ -50,3 +48,17 @@ function change(amount: number, coins: number[]): number {
 //     memo.set(key, pick + notPick); 
 //     return pick + notPick;
 // }
+
+function change(amount: number, coins: number[]): number {
+    const dp = Array(amount + 1).fill(0);
+
+    dp[0] = 1;
+
+    for (const coin of coins) {
+        for(let i = coin; i <= amount; i++) {
+            dp[i] += dp[i - coin];
+        }
+    }
+
+    return dp[amount];
+};
