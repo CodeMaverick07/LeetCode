44c803c378ba2a054bfb20371df384c84ae79053
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2025-03-06 20:25:46 +0530

    Time: 54 ms (68.39%), Space: 84.5 MB (13.99%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.ts
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.ts
@@ -1,18 +1,41 @@
+// function longestCommonSubsequence(text1: string, text2: string): number {
+//    let dp = Array.from({length:text1.length+1},()=>Array(text2.length+1).fill(-1))
+//    return helper(text1,text2,text1.length,text2.length,dp)
+// };
+
+// function helper(text1,text2,i,j,dp){
+//     if (i==0||j==0){
+//         return 0
+//     }
+//     if (dp[i][j]!= -1){
+//         return dp[i][j]
+//     }
+//     if (text1[i-1]==text2[j-1]){
+//         return dp[i][j] = 1 + helper(text1,text2,i-1,j-1,dp)
+//     }
+
+//     return dp[i][j] = Math.max(helper(text1,text2,i-1,j,dp),helper(text1,text2,i,j-1,dp))
+// }
+
 function longestCommonSubsequence(text1: string, text2: string): number {
-   let dp = Array.from({length:text1.length},()=>Array(text2.length).fill(-1))
-   return helper(text1,text2,text1.length-1,text2.length-1,dp)
+    let m = text1.length
+    let n = text2.length
+   let dp = Array.from({length:m+1},()=>Array(n+1).fill(-1))
+   
+   for (let j = 0; j<=n; j++){
+    dp[0][j] = 0
+   }
+   for (let i = 0; i<=m; i++){
+     dp[i][0] = 0
+   }
+   for (let i = 1; i<= m; i++){
+      for (let j = 1; j<= n; j++){
+        if (text1[i-1]==text2[j-1]){
+        dp[i][j] = 1 + dp[i-1][j-1]
+    }
+      else {dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1])}
+    
+    }
+   }
+   return dp[m][n]
 };
-
-function helper(text1,text2,i,j,dp){
-    if (i<0||j<0){
-        return 0
-    }
-    if (dp[i][j]!= -1){
-        return dp[i][j]
-    }
-    if (text1[i]==text2[j]){
-        return dp[i][j] = 1 + helper(text1,text2,i-1,j-1,dp)
-    }
-
-    return dp[i][j] = Math.max(helper(text1,text2,i-1,j,dp),helper(text1,text2,i,j-1,dp))
-}
