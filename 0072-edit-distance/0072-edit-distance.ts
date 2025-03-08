f9277226d915f61e89f69293f8435c1bc03f7ed0
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2025-03-09 01:43:51 +0530

    Time: 7 ms (93.28%), Space: 60.9 MB (38.34%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.ts
+++ 0072-edit-distance/0072-edit-distance.ts
@@ -1,23 +1,45 @@
 function minDistance(word1: string, word2: string): number {
-    let dp = Array.from({ length: word1.length + 1 }, 
-                        () => Array(word2.length + 1).fill(-1));
-    return helper(word1.length, word2.length, word1, word2, dp);
-}
-
-function helper(i: number, j: number, word1: string, word2: string, dp: number[][]): number {
-    if (i === 0) return j; 
-    if (j === 0) return i; 
-
-    if (dp[i][j] !== -1) return dp[i][j]; 
-
-    if (word1[i - 1] === word2[j - 1]) { 
-        return dp[i][j] = helper(i - 1, j - 1, word1, word2, dp);
+    let m = word1.length
+    let n = word2.length
+    let dp = Array.from({ length: m + 1 }, 
+                        () => Array(n + 1).fill(-1));
+    for (let i = 0; i <= m; i++ ){
+        dp[i][0] = i
     }
+    for (let j = 0; j <= n; j++ ){
+        dp[0][j] = j
+    }
+     for (let i = 1; i <= m; i++){
+        for (let j = 1; j <= n; j++){
+            if (word1[i - 1] === word2[j - 1]) { 
+                dp[i][j] = dp[i - 1][j - 1]
+            } else {
+            let insertion = dp[i][j - 1]
+            let deletion = dp[i - 1][ j]
+            let replace = dp[i - 1][j - 1]
+            dp[i][j] = 1 + Math.min(insertion, deletion, replace);
+            }
 
-    let insertion = helper(i, j - 1, word1, word2, dp); 
-    let deletion = helper(i - 1, j, word1, word2, dp); 
-    let replace = helper(i - 1, j - 1, word1, word2, dp); 
-
-    return dp[i][j] = 1 + Math.min(insertion, deletion, replace);
+           
+        }     
+    }
+     return dp[m][n]
 }
+
+// function helper(i: number, j: number, word1: string, word2: string, dp: number[][]): number {
+//     if (i === 0) return j; 
+//     if (j === 0) return i; 
+
+//     if (dp[i][j] !== -1) return dp[i][j]; 
+
+//     if (word1[i - 1] === word2[j - 1]) { 
+//         return dp[i][j] = helper(i - 1, j - 1, word1, word2, dp);
+//     }
+
+//     let insertion = helper(i, j - 1, word1, word2, dp); 
+//     let deletion = helper(i - 1, j, word1, word2, dp); 
+//     let replace = helper(i - 1, j - 1, word1, word2, dp); 
+
+//     return dp[i][j] = 1 + Math.min(insertion, deletion, replace);
+// }
 
