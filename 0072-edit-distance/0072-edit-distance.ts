a7d0d73a0e6c7e04b6df90f5d84b410dc6f3c5b6
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2025-03-09 01:24:53 +0530

    Time: 6 ms (94.86%), Space: 60.6 MB (39.92%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.ts
+++ 0072-edit-distance/0072-edit-distance.ts
@@ -1,25 +1,23 @@
 function minDistance(word1: string, word2: string): number {
-    let dp = Array.from({length:word1.length+1},()=>Array(word2.length+1).fill(-1))
-    return helper(word1.length-1,word2.length-1,word1,word2,dp)
-};
-
-function helper(i,j,word1,word2,dp){
-    if (i == -1){
-        return j + 1
-    }
-    if (j == -1){
-        return i + 1
-    }
-    if ( dp[i][j] != -1 ) {
-        return dp[i][j]
-    }
-    if (word1[i] == word2[j]){
-       return dp[i][j] = helper(i - 1, j - 1, word1, word2, dp);
-    }
-    let insertion =  helper(i-1,j,word1,word2,dp)
-    let deletion =  helper(i,j-1,word1,word2,dp)
-    let replace = helper(i-1,j-1,word1,word2,dp)
-    
-    return dp[i][j] = 1 + Math.min(insertion,deletion,replace)
-
+    let dp = Array.from({ length: word1.length + 1 }, 
+                        () => Array(word2.length + 1).fill(-1));
+    return helper(word1.length, word2.length, word1, word2, dp);
 }
+
+function helper(i: number, j: number, word1: string, word2: string, dp: number[][]): number {
+    if (i === 0) return j; 
+    if (j === 0) return i; 
+
+    if (dp[i][j] !== -1) return dp[i][j]; 
+
+    if (word1[i - 1] === word2[j - 1]) { 
+        return dp[i][j] = helper(i - 1, j - 1, word1, word2, dp);
+    }
+
+    let insertion = helper(i, j - 1, word1, word2, dp); 
+    let deletion = helper(i - 1, j, word1, word2, dp); 
+    let replace = helper(i - 1, j - 1, word1, word2, dp); 
+
+    return dp[i][j] = 1 + Math.min(insertion, deletion, replace);
+}
+
