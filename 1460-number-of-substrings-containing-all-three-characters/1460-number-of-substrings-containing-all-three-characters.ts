5ad7628bda21fe6a465230df461843946a3ec4dc
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2025-03-11 12:23:30 +0530

    Time: 8 ms (96.43%), Space: 57.7 MB (17.86%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.ts
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.ts
@@ -1,16 +1,33 @@
-function numberOfSubstrings(s: string): number {
-    let count = 0;
-    let left = 0;
-    let freq = { 'a': 0, 'b': 0, 'c': 0 };
+// function numberOfSubstrings(s: string): number {
+//     let count = 0
+//     let left = 0
+//     let n = s.length 
+//     let arr = new Map()
+//     arr.set("a",0)
+//     arr.set("b",0)
+//     arr.set("c",0)
+//     for (let i = 0; i<n; i++){
+//         arr.set(s[i],arr.get(s[i])+1)
+//         while(arr.get("a")>0&&arr.get("b")>0&&arr.get("c")>0){
+//           count = count + n - i
+//           arr.set(s[left],arr.get(s[left])-1)
+//           left++
+//         }
+//     }
+//     return count
+// };
 
-    for (let right = 0; right < s.length; right++) {
-        freq[s[right]]++;
-        while (freq['a'] > 0 && freq['b'] > 0 && freq['c'] > 0) {
-            count += s.length - right;
-            freq[s[left]]--;
-            left++;
+function numberOfSubstrings(s: string): number {
+    let hash=new Array(3).fill(-1)
+    let count=0
+
+    for(let i=0;i<s.length;i++){
+        hash[s.charCodeAt(i)-97]=i
+        if(hash[0]!=-1&&hash[1]!=-1&&hash[2]!=-1){
+            let minInd=Math.min(hash[0],Math.min(hash[1],hash[2]))
+            count+=1+minInd
         }
     }
 
-    return count;
-}
+    return count
+};
