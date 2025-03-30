ef6a00be6bcff3b52ef187e59456a9073d5d8b88
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2025-03-30 14:19:11 +0530

    Time: 4 ms (87.6%), Space: 57.3 MB (42.64%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.ts
+++ 0768-partition-labels/0768-partition-labels.ts
@@ -1,23 +1,18 @@
 function partitionLabels(s: string): number[] {
-    let lastIndex = new Map();
-    let ans = [];
-    
-
-    for (let i = 0; i < s.length; i++) {
-        lastIndex.set(s[i], i);
+    let lastIndex = new Map()
+    let ans = []
+    for (let i = 0; i<s.length; i++){
+        lastIndex.set(s[i],i)
     }
-    
-    let start = 0, end = 0;
-    
-
-    for (let i = 0; i < s.length; i++) {
-        end = Math.max(end, lastIndex.get(s[i])); 
-        
-        if (i === end) { 
-            ans.push(end - start + 1); 
-            start = i + 1; 
+    let start = 0
+    let end = 0
+    for (let i = 0; i < s.length; i++){
+        end = Math.max(end,lastIndex.get(s[i]))
+        if (i === end){
+            ans.push(end - start + 1)
+            start = i + 1
         }
+        
     }
-    
-    return ans;
-}
+    return ans
+};
