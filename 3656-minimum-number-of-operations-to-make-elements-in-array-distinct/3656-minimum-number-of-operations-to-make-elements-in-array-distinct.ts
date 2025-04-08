d6fadcc9c9e861ea9e62b7aa08923550eff129d5
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2025-04-09 01:46:50 +0530

    Time: 2 ms (76%), Space: 57.7 MB (68%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.ts
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.ts
@@ -1,13 +1,13 @@
 var minimumOperations = function(nums) {
-    const seen = new Set(); // chakra detector
+    const seen = new Set(); 
 
     for (let i = nums.length - 1; i >= 0; i--) {
         if (seen.has(nums[i])) {
-            // Repeat chakra detected → calculate removal ops
+
             return Math.floor((i + 3) / 3);
         }
         seen.add(nums[i]);
     }
 
-    return 0; // all chakra are distinct
+    return 0; 
 };
