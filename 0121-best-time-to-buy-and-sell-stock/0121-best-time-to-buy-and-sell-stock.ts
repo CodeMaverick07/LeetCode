0f80f8a3616b8cb15c40aa0b663b839af1568323
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2025-07-25 19:11:04 +0530

    Time: 2 ms (79.02%), Space: 65.9 MB (59%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,14 +1,17 @@
 function maxProfit(prices: number[]): number {
-    let minPrice = Infinity;
-    let maxProfit = 0;
+    let min = prices[0];
+    let maxProf = 0;
 
-    for (let i = 0; i<prices.length; i++) {
-        if (prices[i] < minPrice) {
-            minPrice = prices[i];
-        } else {
-            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
-        }
+    for (let index = 1; index < prices.length; index++) {
+            if (prices[index] < min) {
+                min = prices[index];
+            } else if (prices[index] > min) {
+                const profit = prices[index] - min;
+          
+                if (profit > maxProf) maxProf = profit;
+            }
     }
+   
+    return maxProf;
 
-    return maxProfit;
-}
+};
