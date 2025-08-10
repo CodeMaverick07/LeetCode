29e2bed24807d261220167c394540c185594f5c7
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2025-08-10 15:56:50 +0530

    Time: 537 ms (4.52%), Space: 47.7 MB (6.2%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int l1 = word1.length();
+        int l2 = word2.length();
+
+        int[][] costDP = new int[l1+1][l2+1];
+
+        for (int i = 0; i < l1+1; i++){
+            costDP[i][0] = i;
+        }
+        for (int i = 0; i < l2+1; i++){
+            costDP[0][i] = i;
+        }
+
+        for (int i = 1; i < l1+1; i++){
+            for (int j = 1; j < l2+1; j++){
+                System.out.println(word1.charAt(i-1)+" "+word2.charAt(j-1));
+                if (word1.charAt(i-1)==word2.charAt(j-1)){
+                    costDP[i][j] = costDP[i-1][j-1];
+                }else {
+                int topLeft = costDP[i-1][j-1];
+                int top = costDP[i-1][j];
+                int left = costDP[i][j-1];
+                costDP[i][j] = Math.min(topLeft,Math.min(top,left))+1;
+            }
+                
+            } 
+        }
+
+      System.out.println(Arrays.deepToString(costDP));
+        return costDP[l1][l2];
+    }
+}
