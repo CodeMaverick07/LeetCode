27c866358c589d22bbae39b7cdbca85f7b880825
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2026-07-08 19:45:15 +0530

    Time: 59 ms (48.82%), Space: 58.3 MB (95.27%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.ts
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.ts
@@ -1,10 +1,11 @@
 function removeDuplicates(nums: number[]): number {
-    let n = nums.length;
-    let k = 2
-    for (let i = 2; i < n; i++){
-      if (nums[i]!= nums[k-2]){
-        nums[k++] = nums[i]
-      }
+    if (nums.length <= 2) return nums.length;
+    let k = 2;
+    for (let i = 2; i < nums.length; i++) {
+        if (nums[i] !== nums[k - 2]) {
+            nums[k] = nums[i];
+            k++;
+        }
     }
-    return k
-};
+    return k;
+}
