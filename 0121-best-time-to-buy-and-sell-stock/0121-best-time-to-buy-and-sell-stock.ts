1bf7d9303afc26e0b50afe2893a64c0d0d50bd5e
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2026-07-09 20:58:02 +0530

    Time: 1 ms (95.06%), Space: 66.3 MB (24.01%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,17 +1,4 @@
 function maxProfit(prices: number[]): number {
-    //<------------------------ DP_Solution ------------------------>
-    // let dp = Array.from({length:prices.length},()=>Array(prices.length).fill(0))
-    // let ans = 0
-    // for (let i = 0; i < prices.length; i++){
-    //     for (let j = i; j < prices.length; j++){
-    //         dp[i][j] = prices[j]-prices[i]
-    //         if (dp[i][j] > ans ){
-    //             ans = dp[i][j]
-    //         }
-    //     }
-    // }
-    // return ans
-    //<------------------------ DP_Solution ------------------------>
     let min_price = prices[0]
     let max_profit = 0
     for (let i = 1; i < prices.length; i++){
@@ -19,9 +6,7 @@ function maxProfit(prices: number[]): number {
             min_price = prices[i]
             continue;
         }
-        let profit = prices[i]-min_price
-        max_profit = Math.max(max_profit,profit)
+        max_profit = Math.max(max_profit,(prices[i]-min_price))
     }
     return max_profit
-
 };
