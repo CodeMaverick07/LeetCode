54492133f9c0c2c4813f5aee79adcd55d149ea59
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2026-07-09 21:47:15 +0530

    Time: 2 ms (65.26%), Space: 58 MB (43.51%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.ts
+++ 0045-jump-game-ii/0045-jump-game-ii.ts
@@ -1,38 +1,14 @@
 function jump(nums: number[]): number {
-    let l = nums.length;
-    let min = new Array(nums.length).fill(0);
-    let visited = new Array(nums.length).fill(false);
-
+    let jumps = 0;
+    let currentEnd = 0;
+    let far = 0;
     
-
-    for (let i = 0; i<l; i++){
-        let j;
-        for (j = i+1; j < i+1+nums[i]; j++){
-            if(j >= l) {
-                return min[l - 1];
-            }
-            
-            if(visited[j]){
-                min[j] = Math.min(min[i] + 1 , min[j]);
-            }else{
-                min[j] = min[i] + 1;
-            }
-            visited[j] = true;
+    for (let i = 0; i < nums.length-1; i++){
+        far = Math.max(far,i+nums[i])
+        if (i === currentEnd){
+            jumps++
+            currentEnd = far
         }
-        // console.log(i , j);
-        // console.log(min);
-
     }
-    return min[l-1]
+    return jumps
 };
-
-
-// [0 1 1 0 0]
-
-// [0 1 1 2 2]
-//    i
-//             j
-
-// [0 1 1 2 2]
-//    i 
-//      j 
