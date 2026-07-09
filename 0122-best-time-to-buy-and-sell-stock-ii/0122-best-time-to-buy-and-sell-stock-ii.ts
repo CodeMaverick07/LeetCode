90437506f4b794dac9b59f1a19f14a93a88d047e
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2026-07-09 21:13:17 +0530

    Time: 0 ms (100%), Space: 56.1 MB (65.42%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.ts
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.ts
@@ -1,17 +1,13 @@
 function maxProfit(prices: number[]): number {
-   /*
-    let iterate the prices reversely. If [n]>[n-1], we could buy and sell
-
-    [1,2,3]
-    [2,3,1]
-    [3,2,1]
-    [3,1,2]
-  */
-   let profit = 0;
-   for (let i = prices.length - 1; i > 0; i--) {
-      if (prices[i] > prices[i - 1]) {
-         profit += prices[i] - prices[i - 1];
-      }
-   }
-   return profit;
-}
+    let profit = 0;
+    let buyPrice = prices[0]
+    for (let i = 1; i < prices.length; i++){
+        if (buyPrice > prices[i]){
+            buyPrice = prices[i]
+        } else if (buyPrice < prices[i]){
+            profit = profit + (prices[i]-buyPrice)
+            buyPrice = prices[i]
+        }
+    }
+    return profit
+};
