8500a51dfba90ffad594b90d19658e6d3bd00610
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2026-07-10 17:00:19 +0530

    Time: 2 ms (40.39%), Space: 54.1 MB (94.14%) - LeetHub

--- 0274-h-index/0274-h-index.ts
+++ 0274-h-index/0274-h-index.ts
@@ -1,13 +1,9 @@
 function hIndex(citations: number[]): number {
     citations.sort((a,b)=> a - b)
-    console.log(citations)
-    let ans = 0
     for (let i = 0; i < citations.length; i++){
-        
         if (citations[i] >= citations.length - i ){
             return citations.length - i
         }
-        console.log(i,ans)
     }
-    return ans
+    return 0
 };
