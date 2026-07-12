672da81b93585cadfff7c11c622cc5127779066f
Hemant Jatal <111072612+CodeMaverick07@users.noreply.github.com>
2026-07-12 18:31:14 +0530

    Time: 12 ms (5.07%), Space: 57.9 MB (28.5%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.ts
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.ts
@@ -1,36 +1,30 @@
 function reverseWords(s: string): string {
-    let array = [];
-    let word = "";
-    let i = 0;
-
-    while (i < s.length) {
-        if (s[i] != " ") {
-            word += s[i];
-            i++;
+    let array = []
+    let word = ""
+    let i = 0
+    while (i < s.length){
+        if (s[i]!= " "){
+            word = word + s[i]
+            i++
         } else {
-            if (word !== "") {      // push only valid words
-                array.push(word);
-                word = "";
+            while (s[i]==" "){
+                i++
             }
-
-            while (i < s.length && s[i] == " ") {
-                i++;
+            if (word != ""){
+            array.push(word)
+            word = ""
             }
+        
         }
+    
     }
-    if (word !== "") {
-        array.push(word);
+    console.log(array)
+    for (let i = array.length -1; i >= 0; i--){
+    if (word == ""){
+        word = array[i]
+    } else {
+        word = word + " " +array[i]
     }
-
-    word = "";
-
-    for (let i = array.length - 1; i >= 0; i--) {
-        if (word === "") {
-            word = array[i];
-        } else {
-            word += " " + array[i];
-        }
     }
-
-    return word;
-}
+    return word
+};
